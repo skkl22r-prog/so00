3411989553f975409efdb09cd8dc01df999b8683
skkl22r-prog <skkl22r@gmail.com>
2026-07-15 23:38:57 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -102,7 +102,7 @@ border: "1px solid rgba(75,39,55,.18)",
               >
                 <iframe
                   title="Venue"
-                  src="https://www.google.com/maps?q=قاعة+شهرزاد+الطائف&output=embed"
+src="https://www.google.com/maps?q=قصر+النخيل+للاحتفالات+والمؤتمرات+حائل&output=embed"
                   width="100%"
                   height="320"
                   loading="lazy"
