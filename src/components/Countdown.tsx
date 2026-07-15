e3c0ce0d4c544c6d0276b29093980c21f05fd6ef
skkl22r-prog <skkl22r@gmail.com>
2026-07-15 23:36:40 +0300

    Update Countdown.tsx

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useLang } from "@/i18n/LanguageContext";
 
-const TARGET = new Date("2026-09-25T19:30:00+03:00").getTime();
+const TARGET = new Date("2026-08-06T19:30:00+03:00").getTime();
 
 const Countdown = () => {
   const { t } = useLang();
